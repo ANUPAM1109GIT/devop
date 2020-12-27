0fdb0b5afa125d7ea6cdf9a42be79c013a9ca037
Cloud User <ec2-user@ip-172-31-12-83.ap-south-1.compute.internal>
2020-12-27 14:29:58 +0000

    merge 3.sh from local and remote

--- 3.sh
+++ 3.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-<<<<<<< HEAD
 echo "File with same name as 3.sh"
-
-=======
 echo "3.sh file created on git directly"
->>>>>>> 66bcf001d16cb599d274af51fb1f42e2a72988a3
+echo "mergetool merged 3.sh files in local and remote repo
+

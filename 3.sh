fecd9ee02010ff880224b772f267d4b968282939
Cloud User <ec2-user@ip-172-31-12-83.ap-south-1.compute.internal>
2020-12-27 14:30:53 +0000

    Revert "merge 3.sh from local and remote"
    This reverts commit 0fdb0b5afa125d7ea6cdf9a42be79c013a9ca037.

--- 3.sh
+++ 3.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+<<<<<<< HEAD
 echo "File with same name as 3.sh"
-echo "3.sh file created on git directly"
-echo "mergetool merged 3.sh files in local and remote repo
 
+=======
+echo "3.sh file created on git directly"
+>>>>>>> 66bcf001d16cb599d274af51fb1f42e2a72988a3
